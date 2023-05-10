c35237d6483e5690140c36dc8a881489f352e7c5
PabloSFlores <20213tn013@utez.edu.mx>
2023-05-10 11:03:37 -0600

    getOne, save, update

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,16 +1,56 @@
 const { Response } = require("express")
-const pool = require('../utils/postgressql')
+const pool = require("../utils/postgressql")
 
-const getAllEmployees = async(req, res = Response) => {
+const getAllEmployees = async (req, res = Response) => {
     try {
-        const response = await pool.query('SELECT * FROM employees;')
-        return res.status(200).json({data: response.rows})
+        const response = await pool.query("SELECT * FROM employees;")
+        return res.status(200).json({ data: response.rows })
     } catch (error) {
         console.error(error)
-        return res.status(400).json({message:'Internal server error'})
+        return res.status(400).json({ message: "Internal server error" })
+    }
+}
+
+const getOneEmployee = async (req, res = Response) => {
+    try {
+        const id = parseInt(req.params.id)
+        const response = await pool.query("SELECT * FROM employees WHERE id = $1", [id])
+        console.log(response)
+        if(response.rowCount == 0) return res.status(404).json({message: 'Employee not found'})
+        return res.status(200).json({ data: response.rows[0]})
+    } catch (error) {
+        console.error(error)
+        return res.status(400).json({ message: "Internal server error" })
+    }
+}
+
+const saveEmployee = async (req, res = Response) => {
+    try {
+        const { name, surname, lastname, email } = req.body
+        const response = await pool.query("INSERT INTO employees (name, surname, lastname, email) VALUES($1, $2, $3, $4);", [name, surname, lastname, email])
+        return res.status(200).json({message: 'Employee created ', employee: {name: name, surname: surname, lastname: lastname, email: email}})
+    } catch (error) {
+        console.error(error)
+        return res.status(400).json({ message: "Internal server error" })
+    }
+}
+
+const updateEmployee = async(req, res = Response) => {
+    try {
+        const id = req.params.id
+        const {name, surname, lastname, email} = req.body
+        const response = await pool.query('UPDATE employees SET name = $2, surname = $3, lastname = $4, email = $5 WHERE id = $1;', [id, name, surname, lastname, email])
+        console.log(response)
+        return res.status(200).json({message: 'Employee updated', employee: {name: name, surname: surname, lastname: lastname, email: email}})
+    } catch (error) {
+        console.error(error)
+        return res.status(400).json({message: 'Internal server error'})        
     }
 }
 
 module.exports = {
-    getAllEmployees
+    getAllEmployees,
+    getOneEmployee,
+    saveEmployee,
+    updateEmployee
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const { Router } = require("express")
-const { getAllEmployees } = require("../controllers/index.controller")
+const { getAllEmployees, getOneEmployee,saveEmployee, updateEmployee } = require("../controllers/index.controller")
 const router = Router()
 
 router.get('/employees', getAllEmployees)
+router.get('/employees/:id', getOneEmployee)
+router.post('/employees', saveEmployee)
+router.put('/employees/:id', updateEmployee)
 
 module.exports = router;
