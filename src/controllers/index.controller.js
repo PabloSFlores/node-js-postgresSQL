b1c20b2da02b9fc79ab23053aa2d8b740f08ec1d
PabloSFlores <20213tn013@utez.edu.mx>
2023-05-10 12:20:19 -0600

    node js api rest postgresSQL

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -15,9 +15,9 @@ const getOneEmployee = async (req, res = Response) => {
     try {
         const id = parseInt(req.params.id)
         const response = await pool.query("SELECT * FROM employees WHERE id = $1", [id])
-        console.log(response)
-        if(response.rowCount == 0) return res.status(404).json({message: 'Employee not found'})
-        return res.status(200).json({ data: response.rows[0]})
+        // console.log(response)
+        if (response.rowCount == 0) return res.status(404).json({ message: "Employee not found" })
+        return res.status(200).json({ data: response.rows[0] })
     } catch (error) {
         console.error(error)
         return res.status(400).json({ message: "Internal server error" })
@@ -28,23 +28,37 @@ const saveEmployee = async (req, res = Response) => {
     try {
         const { name, surname, lastname, email } = req.body
         const response = await pool.query("INSERT INTO employees (name, surname, lastname, email) VALUES($1, $2, $3, $4);", [name, surname, lastname, email])
-        return res.status(200).json({message: 'Employee created ', employee: {name: name, surname: surname, lastname: lastname, email: email}})
+        return res.status(200).json({ message: "Employee created ", employee: { name: name, surname: surname, lastname: lastname, email: email } })
     } catch (error) {
         console.error(error)
         return res.status(400).json({ message: "Internal server error" })
     }
 }
 
-const updateEmployee = async(req, res = Response) => {
+const updateEmployee = async (req, res = Response) => {
     try {
-        const id = req.params.id
-        const {name, surname, lastname, email} = req.body
-        const response = await pool.query('UPDATE employees SET name = $2, surname = $3, lastname = $4, email = $5 WHERE id = $1;', [id, name, surname, lastname, email])
-        console.log(response)
-        return res.status(200).json({message: 'Employee updated', employee: {name: name, surname: surname, lastname: lastname, email: email}})
+        const id = req.params.ids
+        const { name, surname, lastname, email } = req.body
+        const response = await pool.query("UPDATE employees SET name = $2, surname = $3, lastname = $4, email = $5 WHERE id = $1;", [id, name, surname, lastname, email])
+        // console.log(response)
+        if (response.rowCount == 0) return res.status(404).json({ message: "Employee not found" })
+        return res.status(200).json({ message: "Employee updated", employee: { name: name, surname: surname, lastname: lastname, email: email } })
     } catch (error) {
         console.error(error)
-        return res.status(400).json({message: 'Internal server error'})        
+        return res.status(400).json({ message: "Internal server error" })
+    }
+}
+
+const deleteEmployee = async (req, res = Response) => {
+    try {
+        const id = req.params.id
+        const response = await pool.query("DELETE FROM employees WHERE id = $1", [id])
+        // console.log(response)
+        if (response.rowCount == 0) return res.status(404).json({ message: "Employee not found" })
+        return res.status(200).json({ message: "Employee deleted"})
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({message: 'Internal server error.'})
     }
 }
 
@@ -52,5 +66,6 @@ module.exports = {
     getAllEmployees,
     getOneEmployee,
     saveEmployee,
-    updateEmployee
+    updateEmployee,
+    deleteEmployee
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 const { Router } = require("express")
-const { getAllEmployees, getOneEmployee,saveEmployee, updateEmployee } = require("../controllers/index.controller")
+const { getAllEmployees, getOneEmployee,saveEmployee, updateEmployee, deleteEmployee } = require("../controllers/index.controller")
 const router = Router()
 
 router.get('/employees', getAllEmployees)
 router.get('/employees/:id', getOneEmployee)
 router.post('/employees', saveEmployee)
 router.put('/employees/:id', updateEmployee)
+router.delete('/employees/:id', deleteEmployee)
 
 module.exports = router;
